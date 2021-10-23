472aa8327f37d37ebb2ef28c345b1b743ab1f128
David Alfonso <davalf99@gmail.com>
2021-10-22 21:45:06 -0600

    Finished header (without styling)

--- portfolio/src/App.tsx
+++ portfolio/src/App.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
+import information from './information.json'
 import './styles.css'
 import Header from'./Components/header'
 
 function App() {
   return (
     <div>
-      <Header name='David Alfonso' description='Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.' gitHubLink='1' linkedInLink='1'/>
+      <Header name={information.name} description={information.description} gitHubLink={information.gitHub} linkedInLink={information.linkedIn}/>
     </div>
   );
 }
--- portfolio/src/Components/header.tsx
+++ portfolio/src/Components/header.tsx
@@ -12,27 +12,29 @@ interface HeaderElements {
 
 const Header: FC<HeaderElements> = (props) => {
     return (
-        <div id='header'>
-            <div className='container'>
-                <div className="row">
-                    <div id='infoSection' className='col-md-6 col-sm-12'>
-                        <h2>{props.name}</h2>
-                        <p>{props.description}</p>
+        <div id='header' className='container'>
 
-                        <a href={props.gitHubLink}>
-                            <img className='icon' src={gitLogo} alt="gitlogo" />
-                        </a>
-                        <a href={props.linkedInLink}>
-                            <img className='icon' src={linkedInLogo} alt="gitlogo" />
-                        </a>
-
-                        <button className='resumeBtn'>SEE MY RESUME</button>
-                    </div>
-                    <div  className='col-md-6 col-sm-12'>
-                        <img id='profilePicture' src='https://media-exp1.licdn.com/dms/image/C4E03AQHypkXM04RHhw/profile-displayphoto-shrink_800_800/0/1583432178556?e=1640217600&v=beta&t=N90yfPQblfuTqx0OQqB1CSECMRIsqT3DcVBkTbs05P4' alt='profile' />
-                    </div>
+            <div className="row">
+                <div className='profWrapper col-lg-6 col-sm-12'>
+                    <img id='profilePicture' src='https://media-exp1.licdn.com/dms/image/C4E03AQHypkXM04RHhw/profile-displayphoto-shrink_800_800/0/1583432178556?e=1640217600&v=beta&t=N90yfPQblfuTqx0OQqB1CSECMRIsqT3DcVBkTbs05P4' alt='profile' />
                 </div>
+                
+                <div id='infoSection' className='col-lg-6 col-sm-12'>
+                    <h2 id='nameText'>{props.name}</h2>
+                    <p>{props.description}</p>
+
+                    <a href={props.gitHubLink}>
+                        <img className='icon' src={gitLogo} alt="gitlogo" />
+                    </a>
+                    <a href={props.linkedInLink}>
+                        <img className='icon' src={linkedInLogo} alt="gitlogo" />
+                    </a>
+
+                    <button className='resumeBtn btn btn-dark'>SEE MY RESUME</button>
+                </div>
+
             </div>
+
         </div>
     )
 }
